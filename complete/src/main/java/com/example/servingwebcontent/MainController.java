c12e3f57a1cad8bd6375f2105fdf513af8f38162
vladd1821 <david071521@mail.ru>
2025-01-09 11:48:24 +0300

    9/01 com
    9/01 commit

--- complete/src/main/java/com/example/servingwebcontent/MainController.java
+++ complete/src/main/java/com/example/servingwebcontent/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.sql.PreparedStatement;
+
 @Controller
 public class MainController {
     @Autowired
